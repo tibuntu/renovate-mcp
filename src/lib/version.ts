08605d94e9e7366bd4222c0a5e77460c7d490db6
Timo Hankamer <timo.hankamer@tibuntu.dev>
2026-05-25 22:39:39 +0200

    chore(main): release 0.13.0

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath } from "node:url";
 
-export const SERVER_VERSION = "0.12.1"; // x-release-please-version
+export const SERVER_VERSION = "0.13.0"; // x-release-please-version
 
 export type BuildMode = "local" | "released";
 
